319d9b33d475a807f16d0cf983233004a6c3c089
Graham Hukill <ghukill@gmail.com>
2026-05-11 10:27:37 -0400

    Initial scaffolding of CLI commands
    Why these changes are being introduced:
    
    Before any business logic is introduced, ideally we have the general shape of the
    CLI commands.
    
    How this addresses that need:
    * Adds basic CLI commands: ping, test DSpace connection, and the actual fulltext harvest
    * Best guess at very simple options, though more may get added as we go
    
    Side effects of this change:
    * None
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/USE-553

--- README.md
+++ README.md
@@ -25,3 +25,51 @@ A CLI application for harvesting fulltext from DSpace.
 SENTRY_DSN=### If set to a valid Sentry DSN, enables Sentry exception monitoring. This is not needed for local development.
 WORKSPACE=### Set to `dev` for local development, this will be set to `stage` and `prod` in those environments by Terraform.
 ```
+
+## CLI Commands
+
+### `ping`
+
+```text
+Usage: dfh ping [OPTIONS]
+
+  Emit 'pong' to debug logs and stdout.
+
+Options:
+  --help  Show this message and exit.
+```
+
+### `test-dspace-connection`
+
+```text
+Usage: dfh test-dspace-connection [OPTIONS]
+
+  Test API connection to DSpace.
+
+Options:
+  --help  Show this message and exit.
+```
+
+### `harvest`
+
+```text
+Usage: dfh harvest [OPTIONS]
+
+  Harvest fulltext from DSpace and write to a TIMDEX dataset.
+
+  The argument --datast-location is required, as it provides the TIMDEX
+  records to read metadata from, and the location where fulltext will be
+  written back to.
+
+Options:
+  --dataset-location PATH  TIMDEX dataset location, e.g.
+                           's3://timdex/dataset', to read records from and
+                           write fulltext to.  [required]
+  --run-id TEXT            Run ID of TIMDEX ETL run that identifies records to
+                           harvest fulltext for.
+  --record-limit INTEGER   Maximum records of records to retrieve and harvest
+                           fulltext for.
+  --output-jsonl TEXT      Write harvested fulltext to a local JSONLines file
+                           (primarily for testing).
+  --help                   Show this message and exit.
+```
--- dfh/cli.py
+++ dfh/cli.py
@@ -1,6 +1,6 @@
 import logging
+import time
 from datetime import timedelta
-from time import perf_counter
 
 import click
 
@@ -9,20 +9,94 @@ from dfh.config import configure_logger, configure_sentry
 logger = logging.getLogger(__name__)
 
 
-@click.command()
+@click.group("main")
 @click.option(
-    "-v", "--verbose", is_flag=True, help="Pass to log at debug level instead of info"
+    "-v",
+    "--verbose",
+    is_flag=True,
+    help="Pass to log at debug level instead of info",
 )
-def main(*, verbose: bool) -> None:
-    start_time = perf_counter()
+@click.pass_context
+def main(
+    ctx: click.Context,
+    *,
+    verbose: bool,
+) -> None:
+    """DSpace fulltext harvester CLI."""
+    ctx.ensure_object(dict)
+    ctx.obj["start_time"] = time.perf_counter()
+
     root_logger = logging.getLogger()
     logger.info(configure_logger(root_logger, verbose=verbose))
     logger.info(configure_sentry())
     logger.info("Running process")
 
-    # Do things here!
+    def _log_command_elapsed_time() -> None:
+        elapsed_time = time.perf_counter() - ctx.obj["start_time"]
+        logger.info(
+            "Total time to complete process: %s", str(timedelta(seconds=elapsed_time))
+        )
 
-    elapsed_time = perf_counter() - start_time
-    logger.info(
-        "Total time to complete process: %s", str(timedelta(seconds=elapsed_time))
-    )
+    ctx.call_on_close(_log_command_elapsed_time)
+
+
+@main.command()
+def ping() -> None:
+    """Emit 'pong' to debug logs and stdout."""
+    logger.debug("pong")
+    click.echo("pong")
+
+
+@main.command()
+@click.pass_context
+def test_dspace_connection(
+    ctx: click.Context,
+) -> None:
+    """Test API connection to DSpace."""
+    raise NotImplementedError
+
+
+@main.command()
+@click.pass_context
+@click.option(
+    "--dataset-location",
+    required=True,
+    type=click.Path(),
+    help=(
+        "TIMDEX dataset location, e.g. 's3://timdex/dataset', "
+        "to read records from and write fulltext to."
+    ),
+)
+@click.option(
+    "--run-id",
+    required=False,
+    type=str,
+    help="Run ID of TIMDEX ETL run that identifies records to harvest fulltext for.",
+)
+@click.option(
+    "--record-limit",
+    required=False,
+    type=int,
+    default=None,
+    help="Maximum records of records to retrieve and harvest fulltext for.",
+)
+@click.option(
+    "--output-jsonl",
+    required=False,
+    type=str,
+    default=None,
+    help="Write harvested fulltext to a local JSONLines file (primarily for testing).",
+)
+def harvest(
+    ctx: click.Context,
+    dataset_location: str,
+    run_id: str | None,
+    record_limit: int | None,
+    output_jsonl: str | None,
+) -> None:
+    """Harvest fulltext from DSpace and write to a TIMDEX dataset.
+
+    The argument --datast-location is required, as it provides the TIMDEX records to read
+    metadata from, and the location where fulltext will be written back to.
+    """
+    raise NotImplementedError
